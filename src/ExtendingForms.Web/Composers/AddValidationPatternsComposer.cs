2041215eadcb244e71b38a9ea4ac7ddca5122dc3
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-15 13:20:56 +0200

    Added dutch iban validation pattern

---
+++ src/ExtendingForms.Web/Composers/AddValidationPatternsComposer.cs
@@ -0,0 +1,14 @@
+﻿using ExtendingForms.Web.ValidationPatterns;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+
+namespace ExtendingForms.Web.Composers
+{
+	public class AddValidationPatternsComposer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.FormsValidationPatterns().Append<DutchIbanPattern>();
+		}
+	}
+}
---
+++ src/ExtendingForms.Web/ValidationPatterns/DutchIbanPattern.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Forms.Core.Interfaces;
+
+namespace ExtendingForms.Web.ValidationPatterns
+{
+	public class DutchIbanPattern : IValidationPattern
+	{
+		public string Alias => "dutchIbanPattern";
+		public string LabelKey => string.Empty;
+		public string Name => "Dutch IBAN Number";
+		public string Pattern => "^NL[0-9]{2}[A-Z]{4}[0-9]{10}$";
+		public bool ReadOnly => true;
+	}
+}
