b4da54d5441b632b2932c184f89f0713f6185ee2
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-15 14:30:58 +0200

    Added form validation handler

---
+++ src/ExtendingForms.Web/Composers/FormValidationComposer.cs
@@ -0,0 +1,17 @@
+﻿using ExtendingForms.Web.NotificationHandlers;
+using IbanNet.DependencyInjection.ServiceProvider;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Services.Notifications;
+
+namespace ExtendingForms.Web.Composers
+{
+	public class FormValidationComposer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.Services.AddIbanNet();
+
+			builder.AddNotificationHandler<FormValidateNotification, IbanFormValidationHandler>();
+		}
+	}
+}
---
+++ src/ExtendingForms.Web/NotificationHandlers/IbanFormValidationHandler.cs
@@ -0,0 +1,52 @@
+﻿using IbanNet;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Forms.Core.Services.Notifications;
+
+namespace ExtendingForms.Web.NotificationHandlers
+{
+	public class IbanFormValidationHandler : INotificationHandler<FormValidateNotification>
+	{
+		private readonly IIbanValidator ibanValidator;
+
+		public IbanFormValidationHandler(IIbanValidator ibanValidator)
+		{
+			this.ibanValidator = ibanValidator;
+		}
+
+		public void Handle(FormValidateNotification notification)
+		{
+			if (notification.ModelState.IsValid == false)
+			{
+				return;
+			}
+
+			if (notification.Form.Id.ToString().InvariantEquals("19e1b89c-17b5-4a39-a691-6da11d04e18d") == false)
+			{
+				return;
+			}
+
+			var ibanField = notification.Form.AllFields.FirstOrDefault(x => x.Alias.ToLowerInvariant() == "iban");
+
+			if (ibanField == null)
+			{
+				return;
+			}
+
+			var ibanValue = notification.Context.Request.Form[ibanField.Id.ToString()].ToString();
+
+			if (string.IsNullOrWhiteSpace(ibanValue))
+			{
+				notification.ModelState.AddModelError(ibanField.Id.ToString(), "IBAN is a required field");
+			}
+
+			var result = this.ibanValidator.Validate(ibanValue);
+
+			if (result.IsValid)
+			{
+				return;
+			}
+
+			notification.ModelState.AddModelError(ibanField.Id.ToString(), "IBAN is not valid");
+		}
+	}
+}
