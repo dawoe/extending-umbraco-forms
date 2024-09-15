cf152d5458b2f6df41e36ade747f0b178cb60839
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-15 12:59:15 +0200

    Remove workflows

---
+++ src/ExtendingForms.Web/Composers/ExcludeWorkflowTypesComposer.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+using Umbraco.Forms.Core.Providers.WorkflowTypes;
+
+namespace ExtendingForms.Web.Composers
+{
+	public class ExcludeWorkflowTypesComposer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.FormsWorkflows()
+				.Exclude<Slack>()
+				.Exclude<SlackV2>()
+				.Exclude<SendXsltEmail>()
+				.Exclude<SaveAsUmbracoNode>()
+				.Exclude<SaveAsFile>()
+				.Exclude<PostAsXml>()
+				.Exclude<PostToUrl>()
+				.Exclude<SendEmail>();
+		}
+	}
+}
