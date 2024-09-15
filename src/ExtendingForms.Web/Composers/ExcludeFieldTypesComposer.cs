308c6365a66a38e8675f0ba498fcb48d7ee0f9ae
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-15 12:00:53 +0200

    Exclude field types

---
+++ src/ExtendingForms.Web/Composers/ExcludeFieldTypesComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+using Umbraco.Forms.Core.Providers.FieldTypes;
+
+namespace ExtendingForms.Web.Composers
+{
+	public class ExcludeFieldTypesComposer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.FormsFields().Exclude<Recaptcha2>();
+		}
+	}
+}
