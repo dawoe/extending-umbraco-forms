0edcd5e7a6bd031187de60639e56caf94ac3a4bb
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-17 20:14:58 +0200

    Added departments prevalue source and example form

---
+++ src/ExtendingForms.Web/Composers/AddPrevalueSourceComposer.cs
@@ -0,0 +1,14 @@
+﻿using ExtendingForms.Web.PrevalueSources;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+
+namespace ExtendingForms.Web.Composers
+{
+    public class AddPrevalueSourceComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.FormsFieldPreValueSources().Add<DepartmentsPrevalueSource>();
+        }
+    }
+}
---
+++ src/ExtendingForms.Web/PrevalueSources/DepartmentsPrevalueSource.cs
@@ -0,0 +1,57 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Models;
+
+namespace ExtendingForms.Web.PrevalueSources
+{
+    public class DepartmentsPrevalueSource : FieldPreValueSourceType
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        public DepartmentsPrevalueSource(IUmbracoContextAccessor umbracoContextAccessor)
+        {
+            Id = new Guid("42C8158D-2AA8-4621-B653-6A63C7545768");
+            Name = "Departments";
+            Description = "List of departments managed in the data folder in the content section";
+
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public override List<PreValue> GetPreValues(Field? field, Form? form)
+        {
+            var preValues = new List<PreValue>();
+
+            if (_umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext) == false)
+            {
+                return preValues;
+            }
+
+            if (umbracoContext.Content is null)
+            {
+                return preValues;
+            }
+
+            var departments = umbracoContext.Content.GetAtRoot().FirstOrDefault()?.Descendants<Department>().ToList() ??
+                             new List<Department>();
+
+            foreach (var department in departments)
+            {
+                preValues.Add(new PreValue
+                {
+                    Id = department.Id.ToString(),
+                    Value = department.Id.ToString(),
+                    Caption = department.Name,
+                });
+            }
+
+            return preValues;
+        }
+
+        public override List<Exception> ValidateSettings()
+        {
+            var exceptions = new List<Exception>();
+            return exceptions;
+        }
+    }
+}
