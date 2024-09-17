d5198cae7d8270feae7d0aef86ef2114d58b45f8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-17 21:18:34 +0200

    Added magicstring formatting function

---
+++ src/ExtendingForms.Web/Composers/AddParsedPlaceholderFormattersComposer.cs
@@ -0,0 +1,14 @@
+﻿using ExtendingForms.Web.ParsedPlaceholderFormatters;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+
+namespace ExtendingForms.Web.Composers
+{
+    public class AddParsedPlaceholderFormattersComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.FormsParsedPlaceholderFormatters().Add<PropertyValueParsedPlaceholderFormatter>();
+        }
+    }
+}
---
+++ src/ExtendingForms.Web/ParsedPlaceholderFormatters/PropertyValueParsedPlaceholderFormatter.cs
@@ -0,0 +1,60 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Forms.Core.Interfaces;
+
+namespace ExtendingForms.Web.ParsedPlaceholderFormatters
+{
+    public class PropertyValueParsedPlaceholderFormatter : IParsedPlaceholderFormatter
+    {
+        private readonly IUmbracoContextAccessor contextAccessor;
+
+        public PropertyValueParsedPlaceholderFormatter(IUmbracoContextAccessor contextAccessor) => this.contextAccessor = contextAccessor;
+
+        public string FunctionName => "propertyValue";
+
+        public string FormatValue(string value, string[] args)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            if (args.Length != 1)
+            {
+                return value;
+            }
+
+            var propertyAlias = args.First();
+
+            if (string.IsNullOrWhiteSpace(propertyAlias))
+            {
+                return value;
+            }
+
+            if (int.TryParse(value, out var contentId) == false)
+            {
+                return value;
+            }
+
+            var umbracoContext = this.contextAccessor.GetRequiredUmbracoContext();
+
+            if (umbracoContext.Content is null)
+            {
+                return value;
+            }
+
+            var content = umbracoContext.Content.GetById(contentId);
+
+            if (content is null)
+            {
+                return value;
+            }
+
+            if (content.HasProperty(propertyAlias) == false)
+            {
+                return value;
+            }
+
+            return content.Value<string>(propertyAlias) ?? value;
+        }
+    }
+}
