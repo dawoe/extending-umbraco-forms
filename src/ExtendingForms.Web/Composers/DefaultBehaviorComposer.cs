4cd4eee1a6ad4a43f797f01cf7c2c9f0eeb5d4f2
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-17 17:28:36 +0200

    Apply default behaviors

---
+++ src/ExtendingForms.Web/Composers/DefaultBehaviorComposer.cs
@@ -0,0 +1,14 @@
+﻿using ExtendingForms.Web.DefaultBehaviors;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Web.Behaviors;
+
+namespace ExtendingForms.Web.Composers
+{
+    public class DefaultBehaviorComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.AddUnique<IApplyDefaultFieldsBehavior, ApplyDefaultFieldsBehavior>();
+        }
+    }
+}
---
+++ src/ExtendingForms.Web/DefaultBehaviors/ApplyDefaultFieldsBehavior.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Options;
+using Umbraco.Forms.Core.Configuration;
+using Umbraco.Forms.Core.Models;
+using Umbraco.Forms.Core.Providers;
+using Umbraco.Forms.Web.Behaviors;
+using Umbraco.Forms.Web.Extensions;
+using Umbraco.Forms.Web.Models.Backoffice;
+
+namespace ExtendingForms.Web.DefaultBehaviors
+{
+    public class ApplyDefaultFieldsBehavior : IApplyDefaultFieldsBehavior
+    {
+        private readonly FormDesignSettings _formDesignSettings;
+        private readonly FieldCollection _fieldCollection;
+
+        public ApplyDefaultFieldsBehavior(
+            IOptions<FormDesignSettings> formDesignSettings,
+            FieldCollection fieldCollection)
+        {
+            this._formDesignSettings = formDesignSettings.Value;
+            this._fieldCollection = fieldCollection;
+        }
+
+        public void ApplyDefaultFields(FormDesign form)
+        {
+            Page page = new Page();
+            form.Pages.Add(page);
+            FieldSet fieldSet = new FieldSet()
+            {
+                Id = Guid.NewGuid()
+            };
+            page.FieldSets.Add(fieldSet);
+            FieldsetContainer container = new FieldsetContainer()
+            {
+                Width = 12
+            };
+            fieldSet.Containers.Add(container);
+
+            var firstNameField = new Field
+            {
+                Alias = "firstName",
+                Id = Guid.NewGuid(),
+                FieldTypeId = Guid.Parse(Umbraco.Forms.Core.Constants.FieldTypes.Textfield),
+                Mandatory = true,
+                Caption = "First name"
+            };
+
+            container.Fields.Add(firstNameField);
+
+
+            if (this._formDesignSettings.DisableAutomaticAdditionOfDataConsentField)
+                return;
+            container.AddDataConsentField(this._formDesignSettings, this._fieldCollection);
+        }
+    }
+}
