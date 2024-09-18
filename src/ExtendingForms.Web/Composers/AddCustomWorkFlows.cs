afd1a7d4c2e09c4225c028639108e6123acfa19c
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2024-09-18 18:36:18 +0200

    Added custom workflow example

---
+++ src/ExtendingForms.Web/Composers/AddCustomWorkFlows.cs
@@ -0,0 +1,14 @@
+﻿using ExtendingForms.Web.Workflows;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers.Extensions;
+
+namespace ExtendingForms.Web.Composers
+{
+	public class AddCustomWorkFlows : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			builder.FormsWorkflows().Add<DutchIbanWorkFlowType>();
+		}
+	}
+}
---
+++ src/ExtendingForms.Web/Workflows/DutchIbanWorkFlowType.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Enums;
+
+namespace ExtendingForms.Web.Workflows
+{
+    public class DutchIbanWorkFlowType : WorkflowType
+    {
+	    private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+	    private readonly IHttpContextAccessor _httpContextAccessor;
+	    private readonly ITempDataDictionaryFactory _tempDataDictionaryFactory;
+
+	    public DutchIbanWorkFlowType(IUmbracoContextAccessor umbracoContextAccessor, IHttpContextAccessor httpContextAccessor, ITempDataDictionaryFactory tempDataDictionaryFactory)
+	    {
+		    _umbracoContextAccessor = umbracoContextAccessor;
+		    _httpContextAccessor = httpContextAccessor;
+		    _tempDataDictionaryFactory = tempDataDictionaryFactory;
+
+		    this.Id = new Guid("30008022-e05e-4b05-9348-733498bedbc8");
+		    this.Name = "Dutch IBAN Processor";
+		    this.Description = "Demo work flow for processing form data";
+		    this.Icon = "icon-piggy-bank";
+		    this.Group = "Services";
+		}
+
+	    [Umbraco.Forms.Core.Attributes.Setting("Error page", View = "Pickers.ContentWithXPath", IsMandatory = true)]
+		public string ErrorPage { get; set; }
+
+	    public override Task<WorkflowExecutionStatus> ExecuteAsync(WorkflowExecutionContext context)
+	    {
+			// we are assuming something went wrong in processing here.
+			if (_httpContextAccessor.HttpContext is null)
+			{
+				return Task.FromResult(WorkflowExecutionStatus.Failed);
+			}
+
+			// save error in temp data
+			var tempData = _tempDataDictionaryFactory.GetTempData(_httpContextAccessor.HttpContext);
+			tempData.Add("FormError", "I am not able to process your IBAN");
+
+
+			// set custom error page url here
+			if (_umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext))
+			{
+				if (umbracoContext.Content is not null)
+				{
+					var content = umbracoContext.Content.GetById(int.Parse(this.ErrorPage));
+
+					if (content != null)
+					{
+						_httpContextAccessor.HttpContext.Items[Constants.ItemKeys.RedirectAfterFormSubmitUrl] = content.Url();
+					}
+				}
+			}
+
+			context.Record.State = FormState.Rejected;
+			
+		    return Task.FromResult(WorkflowExecutionStatus.Failed);
+	    }
+
+	    public override List<Exception> ValidateSettings()
+	    {
+		    return new List<Exception>();
+	    }
+    }
+}
